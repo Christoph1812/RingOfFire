b865b952abb3b4935ae808ecf872f26887688faa
Christoph1812 <99895259+Christoph1812@users.noreply.github.com>
2023-08-28 20:35:05 +0200

    app connected to angular firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'ring-of-fire-d0d17',
+    appId: '1:505161061332:web:63e1154f7459b05ecabc41',
+    storageBucket: 'ring-of-fire-d0d17.appspot.com',
+    apiKey: 'AIzaSyBLm7doXYDKCvK36H1NpUf8mdvB80NzAFY',
+    authDomain: 'ring-of-fire-d0d17.firebaseapp.com',
+    messagingSenderId: '505161061332',
+  },
+};
